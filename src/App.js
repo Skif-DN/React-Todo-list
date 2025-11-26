aefa5e67788d1e31bc4405b5da5c70c3b92394f7
Oleksii <a.svtsk@gmail.com>
2025-11-26 17:38:51 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import { useState, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
+import TodoForm from "./components/Todos/TodoForm";
+import TodoList from "./components/Todos/TodoList";
+import TodosActions from "./components/Todos/TodosActions";
+import "./App.css";
+
+function App() {
+  const [todos, setTodos] = useState(() => {
+    const savedTodos = localStorage.getItem("todos");
+    return savedTodos ? JSON.parse(savedTodos) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const addTodoHandler = (text) => {
+    const newTodo = {
+      text: text,
+      isCompleted: false,
+      id: uuidv4(),
+    };
+    setTodos([...todos, newTodo]);
+  };
+
+  const deleteTodoHandler = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const toggleTodoHandler = (id) => {
+    setTodos(
+      todos.map((todo) => {
+        return todo.id === id
+          ? { ...todo, isCompleted: !todo.isCompleted }
+          : { ...todo };
+      })
+    );
+  };
+
+  const resetTodosHandler = () => {
+    setTodos([]);
+    localStorage.removeItem("todos");
+  };
+
+  const deleteCompletedTodosHandler = () => {
+    setTodos(todos.filter((todo) => !todo.isCompleted));
+  };
+
+  const completedTodosCount = todos.filter((todo) => todo.isCompleted).length;
+
+  return (
+    <div className="App">
+      <h1>Todo App</h1>
+      <TodoForm addTodo={addTodoHandler} />
+      {!!todos.length && (
+        <TodosActions
+          completedTodosExist={!!completedTodosCount}
+          resetTodos={resetTodosHandler}
+          deleteCompletedTodos={deleteCompletedTodosHandler}
+        />
+      )}
+
+      <TodoList
+        todos={todos}
+        deleteTodo={deleteTodoHandler}
+        toggleTodo={toggleTodoHandler}
+      />
+
+      {completedTodosCount > 0 && (
+        <h2>{`You have completed ${completedTodosCount} ${
+          completedTodosCount > 1 ? "todos" : "todo"
+        }`}</h2>
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Todos/TodoList.js
@@ -0,0 +1,21 @@
+import Todo from "./Todo";
+import styles from "./TodoList.module.css";
+
+function TodoList({ todos, deleteTodo, toggleTodo, editTodo }) {
+  return (
+    <div className={styles.todoListContainer}>
+      {!todos.length && <h2>Todo list is empty</h2>}
+      {todos.map((todo, index) => (
+        <Todo
+          key={todo.id}
+          todo={todo}
+          deleteTodo={deleteTodo}
+          toggleTodo={toggleTodo}
+          editTodo={editTodo}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default TodoList;
